de0343423dee423b6301068a9f3832afeca56a5c
Zsuzsanna Sandor <zsuzsanna.sandor@gmail.com>
2022-09-14 20:50:18 +0200

    template structure implement

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -6,8 +6,33 @@ from django.http import HttpResponse
 MVC - MVT: Model View Template
 """
 
+posts = [
+    {
+        'author': 'Ricsi Kovács',
+        'title': 'Blog post 1',
+        'content': 'first content',
+        'date_posted': '2022 szeptember 14'
+    },
+    {
+        'author': 'Ricsi Kovács',
+        'title': 'Blog post 2',
+        'content': 'second content',
+        'date_posted': '2022 szeptember 15'
+    },
+    {
+        'author': 'Ricsi Kovács',
+        'title': 'Blog post 3',
+        'content': '3rd content',
+        'date_posted': '2022 szeptember 17'
+    },
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    text = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=text)
 
 def about(request):
     return render(request, 'blog/about.html')
