aa199baeaae296efbdc41cec92679ecde15b4461
Zsuzsanna Sandor <zsuzsanna.sandor@gmail.com>
2022-10-10 17:42:36 +0200

    profile page

--- my_blog/user_registration/views.py
+++ my_blog/user_registration/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import ProfileModel
+from django.contrib.auth.models import User
 
 # MVT - MVC (Djangoban MVT, mert a Template veszi át a Control szerepét)
 #  register oldal
@@ -16,6 +19,12 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            # itt kell létrehoznom a profilt
+            username = form.cleaned_data.get('username')
+            usr_obj = User.objects.filter(username=username).first()
+
+            prof_temp = ProfileModel.objects.create(user_id=usr_obj.pk)
+
             messages.success(request, f"Your account has been created! You are able to login")
             # a login oldalra kellene irányítania
             # valamilyen visszajelzést kellene kapnom, hogy létrejött a userem
@@ -28,3 +37,8 @@ def register(request):
         'form': form
     }
     return render(request, 'user/register.html', context=context)
+
+# @login_required(login_url='login')
+@login_required
+def profile_view(request):
+    return render(request, 'user/profile.html')
