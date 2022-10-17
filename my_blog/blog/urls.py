5270ce30bd9640fa1ed9c1238835e21872398b0d
Zsuzsanna Sandor <zsuzsanna.sandor@gmail.com>
2022-10-17 17:41:15 +0200

    create post and open posts

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import home, about
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView
+                    )
 
 urlpatterns = [
-    path('', home, name="blog-home"),
+    # path('', home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
     path('about/', about, name="blog-about"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
 ]
