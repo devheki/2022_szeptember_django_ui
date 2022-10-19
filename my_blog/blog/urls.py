be86b515d550e8bdc2d1263748839b0b65bfd6eb
Zsuzsanna Sandor <zsuzsanna.sandor@gmail.com>
2022-10-19 20:14:38 +0200

    delete post and implement pagination

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -3,7 +3,8 @@ from .views import home, about
 from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
-                    PostUpdateView
+                    PostUpdateView,
+                    PostDeleteView
                     )
 
 urlpatterns = [
@@ -12,5 +13,6 @@ urlpatterns = [
     path('about/', about, name="blog-about"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
     path('post/new/', PostCreateView.as_view(), name="post-create"),
 ]
